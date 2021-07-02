eae399b4f0ea7dadb9b95be89f976b78820ddb48
Yash Kapoor <yashkapoor.dram@gmail.com>
2021-07-02 10:15:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { NavBar } from "./MyComponent/NavBar";
+import { Header } from "./MyComponent/Header";
+import { Home } from "./MyComponent/Home";
+import { Menu } from "./MyComponent/Menu";
+import { About } from "./MyComponent/About";
+import { useState } from "react";
+
+//importing the images
+import homeImg from "./Images/1.png";
 
 function App() {
+  const [menu, setMenu] = useState(true);
+  const ToggleMenu = () => {
+    setMenu(!menu);
+    console.log(menu);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route
+            exact path="/"
+            render={() => {
+              return <>This is the home page.</>;
+            }}
+          ></Route>
+          <Route
+            exact path="/About"
+            render={() => {
+              return <>This is the about page.</>;
+            }}
+          ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
